deca56964eacfba6533912d9828f500c5f237dd1
Foram <foramthakral.tech@gmail.com>
2025-08-25 23:29:27 +0530

    TDD Step 2: Test and impl for single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(subject.add('')).to eq(0)
     end
+
+    it 'returns the number for a single number string' do
+      expect(subject.add('1')).to eq(1)
+    end
   end
 end
